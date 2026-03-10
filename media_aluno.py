13f563cb5f51279f2fbf16196119ed12cb0c814d
AndyMarksss <andersondstv@gmail.com>
2026-03-10 20:49:21 -0300

    Anderson Barbosa Up3

---
+++ media_aluno.py
@@ -0,0 +1,14 @@
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+
+media = (nota1 + nota2) / 2
+
+print("Média:", media)
+
+if media >= 7:
+    print("Aprovado")
+else:
+    if media >= 5:
+        print("Recuperação")
+    else:
+        print("Reprovado")
